241c592fcec85fe6f0feacaf7fe946702bba04b5
root <root@localhost.localdomain>
2020-02-28 21:01:03 -0700

    added in trace

--- nmap_to_csv.sh
+++ nmap_to_csv.sh
@@ -2,7 +2,7 @@
 echo "Scanning:" $1
 nmap -T4 -A -v -oX nmap_output.xml $1
 echo "Finished nmap, starting awk"
-awk -F\" '/<port protocol/ {print $4","$2","$6","}; /<os>/{port=$6;protocol=$4;status=$2}; /<osmatch/ {print port","protocol","status","$2}' nmap_output.xml > nmap_csv.csv
+awk -F\" '/<port protocol/{print $4","$2","$6",,,"};/<os>/{port=$6;protocol=$4;status=$2};/<osmatch/{print port","protocol","status","$2",,"};/<trace/{np=$2;pro=$4};/<hop/{print np","pro",,,"$2","$4}' nmap_output.xml > nmap_csv.csv
 echo "Your file is ready"
 #rm -f nmap_output.xml
 cat nmap_csv.csv
