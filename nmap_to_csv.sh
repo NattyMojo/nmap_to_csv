2ce77237daff5ff642c769996b2ddb5cf1bc37dd
Zach Luciano <zachary.luciano@gmail.com>
2020-02-28 20:41:25 -0700

    refined changes

--- nmap_to_csv.sh
+++ nmap_to_csv.sh
@@ -2,7 +2,8 @@
 echo "Scanning:" $1
 nmap -T4 -A -v -oX nmap_output.xml $1
 echo "Finished nmap, starting awk"
-awk  '/<port protocol/ {print $2","$3","$4}' nmap_output.xml > nmap_csv.csv
+awk -F\" '/<port protocol/ {print $4","$2","$6","}; /<os>/{port=$6;protocol=$4;status=$2}; /<osmatch/ {print port","protocol","status","$2}' nmap_output.xml > nmap_csv.csv
 echo "Your file is ready"
-rm -f nmap_output.xml
+#rm -f nmap_output.xml
 cat nmap_csv.csv
+
